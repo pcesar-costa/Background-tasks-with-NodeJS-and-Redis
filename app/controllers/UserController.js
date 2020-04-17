61d075c0175c587cf2a414f70eff2149d68aa30c
Pedro Costa <pcesar_costa@hotmail.com>
2020-04-17 17:20:58 -0300

    create-user-controller-and-add-queue

---
+++ app/controllers/UserController.js
@@ -0,0 +1,19 @@
+import passwordGenerator from 'password-generator';
+import Queue from '../lib/Queue';
+
+class UserController {
+    async store(req, res) {
+        const { name, email } = req.body;
+        const user = {
+            name,
+            email,
+            pass: passwordGenerator(32, false)
+        };
+
+        await Queue.add('RegistrationMail', { user });
+
+        return res.json(user);
+    }
+}
+
+export default new UserController();
