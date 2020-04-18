06f3f6c25ac561d2917cee0acdd4f40b51585038
Pedro Costa <pcesar_costa@hotmail.com>
2020-04-17 22:52:29 -0300

    Update server.js with Sentry

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
 import "dotenv/config";
 import express from "express";
+import * as Sentry from '@sentry/node';
 import UserController from '../app/controllers/UserController'
 import BullBoard from 'bull-board';
 import Queue from '../app/lib/Queue';
 
 const app = express();
+
+Sentry.init({ dsn: process.env.SENTRY_DNS });
 BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
+
+app.use(Sentry.Handlers.requestHandler());
 
 app.use(express.json());
 app.use('/admin/queues', BullBoard.UI);
 
 app.post('/users', UserController.store);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on the ${process.env.PORT} port`);
+// function to test Sentry
+app.get('/debug-sentry', function mainHandler(req, res) {
+    throw new Error('My first Sentry error!');
+  });
+
+app.use(Sentry.Handlers.errorHandler());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on the ${PORT} port`);
 });
