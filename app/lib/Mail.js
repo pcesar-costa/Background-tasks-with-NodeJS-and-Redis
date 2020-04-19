dbeebbdc92606c8a179647e87326ccf0e3ecc73e
Pedro Costa <pcesar_costa@hotmail.com>
2020-04-19 12:12:26 -0300

    update mail.js - add mail gun and remove nodemailer

--- app/lib/Mail.js
+++ app/lib/Mail.js
@@ -1,4 +1,7 @@
-import nodemailer from 'nodemailer';
-import mailCredentials from '../config/mail';
+// import nodemailer from 'nodemailer';
+// import mailCredentials from '../config/mail';
+// export default nodemailer.createTransport(mailCredentials)
 
-export default nodemailer.createTransport(mailCredentials)
+import mailgun from 'mailgun-js';
+import mailGunCredentials from '../config/mailgun';
+export default mailgun(mailGunCredentials);
