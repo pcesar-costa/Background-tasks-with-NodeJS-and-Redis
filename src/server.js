ec033d7648da8491c16f1f820cf408ef7fae222b
Pedro Costa <pcesar_costa@hotmail.com>
2020-04-19 09:36:32 -0300

    Update-server-js-with-sentry-file-'../app/config/sentry'

--- src/server.js
+++ src/server.js
@@ -1,26 +1,25 @@
 import "dotenv/config";
 import express from "express";
-import * as Sentry from '@sentry/node';
-import UserController from '../app/controllers/UserController'
 import BullBoard from 'bull-board';
+
+import Sentry from '../app/config/sentry';
+import UserController from '../app/controllers/UserController'
 import Queue from '../app/lib/Queue';
 
 const app = express();
 
-Sentry.init({ dsn: process.env.SENTRY_DNS });
 BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
 
 app.use(Sentry.Handlers.requestHandler());
 
 app.use(express.json());
 app.use('/admin/queues', BullBoard.UI);
-
 app.post('/users', UserController.store);
 
 // function to test Sentry
 app.get('/debug-sentry', function mainHandler(req, res) {
     throw new Error('My first Sentry error!');
-  });
+});
 
 app.use(Sentry.Handlers.errorHandler());
 
